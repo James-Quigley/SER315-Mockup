0e64f00b7f8e6bef2431ddc745f716196e01b0cf
James Quigley <james@quigley.us>
2016-10-19 14:31:31 -0700

    Update index.js
    https

--- js/index.js
+++ js/index.js
@@ -6,6 +6,6 @@ $(document).ready(function () {
 $(".movie").click(function(e){
     $(".content iframe").remove();
     $('<iframe width="100%" height="100%" frameborder="0" allowfullscreen autoplay></iframe>')
-    .attr("src", "http://www.youtube.com/embed/" + $(this).attr("movieSrc") + "?autoplay=1")
+    .attr("src", "https://www.youtube.com/embed/" + $(this).attr("movieSrc") + "?autoplay=1")
     .appendTo(".content");
 });
